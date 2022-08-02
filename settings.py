d436ab3274a0d8f130d87496ef44e265f65fbe86
Sergey Minakow <100860484+Erl1x@users.noreply.github.com>
2022-08-02 15:34:43 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,6 @@
+link_login = 'https://petfriends.skillfactory.ru/login'
+
+valid_email = 'elektronnayapochta@yandex.ru'
+valid_password = 'qwerty12345'
+
+link_chromedriver = 'D:/chrome_driver/chromedriver.exe'
